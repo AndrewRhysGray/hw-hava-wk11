ad1268308c7fefcb21f85ed864016c3503858590
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-02-12 09:12:48 +0000

    homework not done

---
+++ src/main/java/Library.java
@@ -0,0 +1,12 @@
+public class Library {
+    private String title;
+    private String author;
+    private String genre;
+
+    public Library(String title, String author, String genre){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.print.Book;
+
+public class LibraryTest {
+    public Library library;
+            public Book book;
+    @Before
+    public void before(){
+        library = new Library("We Need To Talk About Kevin", "Lionel Shriver" );
+    }
+
+    @Test
+    public void
+}
